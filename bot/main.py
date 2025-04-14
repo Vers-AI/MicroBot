aebd8f1baf3cb4bd20cbdbc95785ada92bfc49be
Drekken <drekken.pownz@protonmail.com>
2025-04-14 19:36:42 -0400

    Made Marine and Maurader Squad

--- bot/main.py
+++ bot/main.py
@@ -1,56 +1,53 @@
 from typing import Optional
 
-from ares import AresBot
+from ares.consts import UnitRole
+from ares.behaviors.combat import CombatManeuver
+from ares.behaviors.combat.group import AMoveGroup, StutterGroupBack
 
+from ares import AresBot
+from sc2.ids.unit_typeid import UnitTypeId
+from sc2.unit import Unit
+from sc2.units import Units
+from sc2.position import Point2
+
+import numpy as np
 
 class MyBot(AresBot):
+    BIO_TYPE: set[UnitTypeId] = {UnitTypeId.MARINE, UnitTypeId.MARAUDER}
     def __init__(self, game_step_override: Optional[int] = None):
-        """Initiate custom bot
-
-        Parameters
-        ----------
-        game_step_override :
-            If provided, set the game_step to this value regardless of how it was
-            specified elsewhere
-        """
         super().__init__(game_step_override)
-
+    
+  
     async def on_step(self, iteration: int) -> None:
         await super(MyBot, self).on_step(iteration)
 
-        # step logic here ...
-        pass
+        bio_force: Units = self.mediator.get_units_from_role(role=UnitRole.ATTACKING)
 
-    """
-    Can use `python-sc2` hooks as usual, but make a call the inherited method in the superclass
-    Examples:
-    """
-    # async def on_start(self) -> None:
-    #     await super(MyBot, self).on_start()
-    #
-    #     # on_start logic here ...
-    #
-    # async def on_end(self, game_result: Result) -> None:
-    #     await super(MyBot, self).on_end(game_result)
-    #
-    #     # custom on_end logic here ...
-    #
-    # async def on_building_construction_complete(self, unit: Unit) -> None:
-    #     await super(MyBot, self).on_building_construction_complete(unit)
-    #
-    #     # custom on_building_construction_complete logic here ...
-    #
-    # async def on_unit_created(self, unit: Unit) -> None:
-    #     await super(MyBot, self).on_unit_created(unit)
-    #
-    #     # custom on_unit_created logic here ...
-    #
-    # async def on_unit_destroyed(self, unit_tag: int) -> None:
-    #     await super(MyBot, self).on_unit_destroyed(unit_tag)
-    #
-    #     # custom on_unit_destroyed logic here ...
-    #
-    # async def on_unit_took_damage(self, unit: Unit, amount_damage_taken: float) -> None:
-    #     await super(MyBot, self).on_unit_took_damage(unit, amount_damage_taken)
-    #
-    #     # custom on_unit_took_damage logic here ...
+        if bio_force:
+            self._bio_ball(bio_force, self.game_info.map_center)
+
+    async def on_unit_created(self, unit: Unit) -> None:
+        await super().on_unit_created(unit)
+        if unit.type_id in self.BIO_TYPE:
+            self.mediator.assign_role(
+                tag=unit.tag, role=UnitRole.ATTACKING
+            )
+    
+    def _bio_ball(self, bio_forces: Units, target: Point2) -> None:
+        formation_a: CombatManeuver = CombatManeuver()
+        formation_a.add(
+            AMoveGroup(
+                group=bio_forces,
+                group_tags={r.tag for r in bio_forces},
+                target=target,
+            )
+        )
+        self.register_behavior(formation_a)
+        
+
+        
+
+
+        
+
+   
